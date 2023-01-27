978d449eec5010d756fae7c6f86b06e3a6389752
kamel beloula <kamel.solo@live.com>
2023-01-27 22:57:04 +0000

    added variables and an eventlistner with an ajax request

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,40 +1,39 @@
-alert("hello world!");
-
 // This is our API key
 var APIKey = "166a433c57516f51dfab1f7edaed8413";
-
+// taking the user input and allocate it to a variable
+let inputGroup = $(".input-group-append");
 // Here we are building the URL we need to query the database
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?" +
-  "q=Bujumbura,Burundi&appid=" + APIKey;
+$("#search-button").on("click", function(event) {
 
-// Here we run our AJAX call to the OpenWeatherMap API
-$.ajax({
-  url: queryURL,
-  method: "GET"
-})
-  // We store all of the retrieved data inside of an object called "response"
-  .then(function(response) {
+    // event.preventDefault() can be used to prevent an event's default behavior.
+    // Here, it prevents the submit button from trying to submit a form when clicked
+    event.preventDefault();
+  
+    // Here we grab the text from the input box
+    let searchInput = $("#search-input").val();
+  
+    // Here we construct our URL
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?" +
+  "q=" +  searchInput + "&appid=" + APIKey;
 
-    // Log the queryURL
-    console.log(queryURL);
-
-    // Log the resulting object
-    console.log(response);
-
-    // Transfer content to HTML
-    $(".city").html("<h1>" + response.name + " Weather Details</h1>");
-    $(".wind").text("Wind Speed: " + response.wind.speed);
-    $(".humidity").text("Humidity: " + response.main.humidity);
-    
-    // Convert the temp to Celsius
-    var tempC = response.main.temp - 273.15;
-
-    // add temp content to html
-    $(".temp").text("Temperature (K) " + response.main.temp);
-    $(".tempC").text("Temperature (C) " + tempC.toFixed(2));
-
-    // Log the data in the console as well
-    console.log("Wind Speed: " + response.wind.speed);
-    console.log("Humidity: " + response.main.humidity);
-    console.log("Temperature (C): " + tempC);
+    // Write code between the dashes below to hit the queryURL with $ajax, then take the response data
+    // and display it in the div with an id of movie-view
+  
+    // ------YOUR CODE GOES IN THESE DASHES.
+  
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function(response) {
+        let cityButton = $("<button>").text(response.name);
+  
+        let windButton = $("<button>").text(response.wind.speed); 
+        
+        let humidiyButton = $("<button>").text(response.main.humidity); 
+        
+        inputGroup.append(cityButton, windButton, humidiyButton);
+    });
+  
+    // -----------------------------------------------------------------------
+  
   });
