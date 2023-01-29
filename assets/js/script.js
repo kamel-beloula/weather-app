2eb1bfabc95b8bc31180c3d7822dc124f13070a4
kamel beloula <kamel.solo@live.com>
2023-01-29 11:30:49 +0000

    added a card for the main current weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 // This is our API key
 var APIKey = "166a433c57516f51dfab1f7edaed8413";
 // taking the user input and allocate it to a variable
-let inputGroup = $(".input-group-append");
+let history = $("#history");
 // Here we are building the URL we need to query the database
 $("#search-button").on("click", function(event) {
 
@@ -11,30 +11,58 @@ $("#search-button").on("click", function(event) {
   
     // Here we grab the text from the input box
     let searchInput = $("#search-input").val();
+    let todayEl =$("#today");
+    let forecastEl =$("#forecast");
   
     // Here we construct our URL
     var queryURL = "https://api.openweathermap.org/data/2.5/weather?" +
-  "q=" +  searchInput + "&appid=" + APIKey;
+  "q=" +  searchInput + "&cnt=5&appid=" + APIKey;
 
     // Write code between the dashes below to hit the queryURL with $ajax, then take the response data
     // and display it in the div with an id of movie-view
   
+    
     // ------YOUR CODE GOES IN THESE DASHES.
-  
+  historyArray = [];
+  console.log(historyArray);
     $.ajax({
       url: queryURL,
       method: "GET"
     }).then(function(response) {
-        let cityButton = $("<button>").text(response.name);
-  
-        let windButton = $("<button>").text(response.wind.speed); 
-        
-        let humidiyButton = $("<button>").text(response.main.humidity); 
-        
-        inputGroup.append(cityButton, windButton, humidiyButton);
+       console.log(response);
+            todayEl.append([
+                $('<div>').attr("class","row").append([
+                    $('<div>').attr("class", 'col').append([
+                        $('<div>').attr("class",'card rounded border border-secondary').append([
+                            $('<div>').attr("class", "card-body").append([
+                                $('<h2>').attr('class', 'card-title font-weight-bold').text("city:  " + response.name).append($('<img>')),
+                                $('<h5>').attr('class', 'card-text').text("temperature:  " + response.main.humidity),
+                                $('<h5>').attr('class', 'card-text').text("wind speed:  " + response.wind.speed),
+                                $('<h5>').attr('class', 'card-text').text("humidity:  " + response.main.humidity)
+                            ])
+                        ])
+                    ])
+                ])
+            ]);
+            forecastEl.append(
+                $('<div>', { class: 'col' }).append(
+                    $('<div>', { class: 'card forecast' }).append(
+                        $('<div>', { class: 'card-body' }).append([
+                            $('<h4>').attr('class','card-title').append($('<img>')),
+                            $('<p>').attr('class', 'card-text').text("temperature:  " + response.main.humidity),
+                            $('<p>').attr('class', 'card-text').text("wind speed:  " + response.wind.speed),
+                            $('<p>').attr('class', 'card-text').text("humidity:  " + response.main.humidity)
+                        ])
+                    )
+                )
+            );
+      
+        history.append($("<button>").attr("class","btn").text(response.name));
+
+        localStorage.setItem(searchInput, response.name);
     });
     searchInput = $("#search-input").val("");
-  
+    console.log(historyArray);
     // -----------------------------------------------------------------------
   
   });
