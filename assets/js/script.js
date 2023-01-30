ff17baf5fa8997fbbf86b4c19a139f592b3d78f9
kamel beloula <kamel.solo@live.com>
2023-01-30 23:28:02 +0000

    added html tags for the 5 days forecast row

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,18 +11,12 @@ cityList.forEach(renderCityButtons);
 $("#search-button").on("click", function (event) {
   event.preventDefault();
   currentWeather();
-  weatherForecast()
+  weatherForecast();
 });
 
 function currentWeather() {
   let searchInput = $("#search-input").val().trim();
-  var queryURL =
-    "https://api.openweathermap.org/data/2.5/weather?" +
-    "q=" +
-    searchInput +
-    "&cnt=5&unit=metric&appid=" +
-    APIKey +
-    "&units=metric";
+  var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + searchInput +"&cnt=5&unit=metric&appid=" + APIKey +"&units=metric";
 
   cityList.unshift(searchInput);
   localStorage.setItem("history", JSON.stringify(cityList));
@@ -34,30 +28,12 @@ function currentWeather() {
   }).then(function (response) {
     let iconCode = response.weather[0].icon;
     todayEl.empty();
-    todayEl.prepend([
-      $("<div>")
-        .attr("class", "row")
-        .append([
-          $("<div>")
-            .attr("class", "col")
-            .append([
-              $("<div>")
-                .attr("class", "card rounded border border-secondary mb-2")
-                .append([
-                  $("<div>")
-                    .attr("class", "card-body")
-                    .append([
-                      $("<h2>")
-                        .attr("class", "card-title font-weight-bold")
-                        .text(response.name + " on " + currentDate)
-                        .append(
-                          $("<img>").attr(
-                            "src",
-                            "https://openweathermap.org/img/wn/" +
-                              iconCode +
-                              "@2x.png"
-                          )
-                        ),
+    todayEl.prepend([$("<div>").attr("class", "row").append([
+          $("<div>").attr("class", "col").append([
+              $("<div>").attr("class", "card rounded border border-secondary mb-2").append([
+                  $("<div>").attr("class", "card-body").append([
+                      $("<h2>").attr("class", "card-title font-weight-bold").text(response.name + " on " + currentDate).append(
+                          $("<img>").attr("src","https://openweathermap.org/img/wn/" +iconCode +"@2x.png")),
                       $("<h5>")
                         .attr("class", "card-text")
                         .text("temperature:  " + response.main.temp + " C°"),
@@ -72,14 +48,13 @@ function currentWeather() {
             ]),
         ]),
     ]);
-
     renderCityButtons(searchInput);
+    weatherForecast();
   });
 }
 
 function renderCityButtons() {
-  $("#history").empty();
-  searchInput = $("#search-input").val("");
+  
   cityList.forEach(function (searchInput) {
     if (searchInput){
         $("#history").append(
@@ -89,25 +64,30 @@ function renderCityButtons() {
   });
 }
 
-// forecastEl.append(
-//     $('<div>', { class: 'col' }).append(
-//         $('<div>', { class: 'card forecast' }).append(
-//             $('<div>', { class: 'card-body' }).append([
-//                 $('<h4>').attr('class','card-title').append($('<img>')),
-//                 $('<p>').attr('class', 'card-text').text("temperature:  " + response.main.humidity),
-//                 $('<p>').attr('class', 'card-text').text("wind speed:  " + response.wind.speed),
-//                 $('<p>').attr('class', 'card-text').text("humidity:  " + response.main.humidity)
-//             ])
-//         )
-//     )
-// );
-
-// -----------------------------------------------------------------------
-
-// $("#history").on("click","button", function(event) {
-//     event.preventDefault();
-//     currentWeather();
-// })
+function weatherForecast() {
+  
+  let queryURL = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + lat +'&lon=' + lon +'&appid=' + myApiKey;
+  $.ajax({url: queryURL,method: "GET",}).then(function (response) {
+    let iconCode = response.weather[0].icon;
+  $("#history").empty();
+  searchInput = $("#search-input").val("");
+  $("#forecast").empty();
+  for (var i = 1; i <6 ; i++) {    
+      forecastEl.append(
+    $('<div>', { class: 'col' }).append(
+        $('<div>', { class: 'card forecast' }).append(
+            $('<div>', { class: 'card-body' }).append([
+                $('<h4>').attr('class','card-title').append($('<img>')),
+                $('<p>').attr('class', 'card-text').text(response.daily[i].weather[0].main),
+                $('<p>').attr('class', 'card-text').text(response.wind.speed),
+                $('<p>').attr('class', 'card-text').text(response.daily[i].humidity)
+            ])
+        )
+    )
+);
+  }
+})
+}
 
 historyEl.on('click', '.btn-secondary', function (event) {
     event.preventDefault();
@@ -118,52 +98,3 @@ historyEl.on('click', '.btn-secondary', function (event) {
     console.log(city);
     currentWeather(city);
 })
-
-function weatherForecast(){
-    let fiveDaysURL = "https://api.openweathermap.org/data/2.5/weather?" +"q=" + searchInput + "&cnt=5&unit=metric&appid=" + APIKey +
-    "&units=metric";
-$.ajax({
-    url: fiveDaysURL,
-    method:'get'
-}).then(function (response){
-    let forecastDay = moment()//making sure i have 5 days forecast headings
-    let y = 0 //this is variable I am using to increse the date in moment js inside the loloop later on
-    forecastRow.empty()//clearing up old
-    let headingMain = $('<h2>');//creating heading for the forcast row
-    headingMain.text('Five days forecast : ').addClass('w-100 mb-2')//heading width was set to 100% in bootstrap so it doesnt mess around
-    forecastRow.append(headingMain)
-for (let i = 3; i < 40; i += 8 ){
-    //index increasing by 8, because in the response object there is information for 5 days for every 3 hours, 8*3= 24 so it adds up to a day
-    //creating HTML elements
-    y +=1 //this will increase date (together with line bellow)
-    forecastDay = moment().add(y, 'day').format('DD/MM/YYYY')
-    let iconID = response.list[i].weather[0].icon
-    let fcIconSource = 'https://openweathermap.org/img/wn/' + iconID + '@2x.png' //creating url for icon based on in-object information
-    let fcIcon = $('<img>');
-    fcIcon.attr('src',fcIconSource)//adding the newly made url as src attribute in image
-    fcIcon.attr('alt', 'icon matching weather forecast');
-    let forecastBox = $('<article>')
-    //accessing values form the object
-    let fcTemp = response.list[i].main.temp;
-    fcTemp -= 273.15;
-    fcTemp = fcTemp.toFixed(2);
-    let fcWind = response.list[i].wind.speed;
-    fcWind /= 1.609344;
-    fcWind = fcWind.toFixed(2);
-    let fcHumidity = response.list[i].main.humidity;
-    
-//creating html elements
-    let fcheading = $('<h3>')
-    fcheading.text(forecastDay)
-    let forecastTemp = $('<p>');
-    forecastTemp.text(`Temp: ${fcTemp}°C`); 
-    let forecastWind = $('<p>');
-    forecastWind.text(`Wind: ${fcWind}MPH`);
-    let forecastHum = $('<p>');
-    forecastHum.text(`Humidity: ${fcHumidity}%.`)
-    forecastBox.addClass('bg-info m-1 p-1 rounded text-center text-dark border border-dark col-lg-2 col-sm-4 col-md-4 col-10 align-items-center text-wrap')
-    forecastBox.append(fcIcon, fcheading,  forecastTemp, forecastWind, forecastHum)
-    forecastRow.append(forecastBox)
-}
-})
-}
