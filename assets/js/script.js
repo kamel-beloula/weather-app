4e64978d79981d6825875a9f2f3815ead158c180
kamel beloula <kamel.solo@live.com>
2023-03-03 22:52:16 +0000

    working on the forcast cards, the cards display but not finished

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,7 +11,6 @@ cityList.forEach(renderCityButtons);
 $("#search-button").on("click", function (event) {
   event.preventDefault();
   currentWeather();
-  weatherForecast();
 });
 
 function currentWeather() {
@@ -70,22 +69,64 @@ function weatherForecast() {
   var apiKey = "166a433c57516f51dfab1f7edaed8413";
   var forecast ="https://api.openweathermap.org/data/2.5/forecast?q=" +searchInput +"&units=metric&appid=" + apiKey;
   $.ajax({
-    url: forecast,method: "GET",}).then(function (response) {console.log(response);
-    historyEl.empty();
-    var weatherArray = response.list;
-    for (var i = 0; i < weatherArray.length; i++) {
-      console.log(weatherArray[i]);
-      if (weatherArray[i].dt_txt.split(" ")[1] === "12:00:00") {
-        for (var i = 1; i <6 ; i++) {    
-                forecastEl.append(
-              $('<div>', { class: 'col' }).append(
-                  $('<div>', { class: 'card forecast' }).append(
-                      $('<div>', { class: 'card-body' }).append([
-                          $('<h4>').attr('class','card-title').append($('<img>').attr("src","http://openweathermap.org/img/w/" +weatherArray[i].weather[0].icon +".png")),
-                          $('<p>').attr('class', 'card-text').text(response.daily[i].weather[0].main),
-                          $('<p>').attr('class', 'card-text').text(response.wind.speed),
-                          $('<p>').attr('class', 'card-text').text(response.daily[i].humidity)
-      ]))))}}}})}
+    url: forecast,
+    method: "GET"
+}).then(function(data) {        
+  $("#history").empty();
+  $("#forecast").empty();
+    for (let i = 8; i < data.list.length; i++) {
+        if (i % 8 === 0 || i === data.list.length - 1) {
+          //   for (var i = 1; i <6 ; i++) {    
+//       forecastEl.append(
+//     $('<div>', { class: 'col' }).append(
+//         $('<div>', { class: 'card forecast' }).append(
+//             $('<div>', { class: 'card-body' }).append([
+//                 $('<h4>').attr('class','card-title').append($('<img>').addClass(`icon`).attr(`src`, `https://openweathermap.org/img/wn/` + data.list[i].weather[0].icon + `@2x.png`).attr(`alt`, data.list[i].weather[0].description)),
+//                 $('<p>').attr('class', 'card-text').text(response.daily[i].weather[0].main),
+//                 $('<p>').attr('class', 'card-text').text(response.wind.speed),
+//                 $('<p>').attr('class', 'card-text').text(`Humidity: ` + data.list[i].main.humidity + ` %`)
+//             ])
+//         )
+//     )
+            var date = moment().add(i, 'day');
+            var tempC = data.list[i].main.temp - 273.15; // converts Kelvin to Celsius
+            var icon = data.list[i].weather[0].icon; // pulls icon code in
+            var iconTxt = data.list[i].weather[0].description // pulls in weather description
+            var iconImg = `https://openweathermap.org/img/wn/` + icon + `@2x.png`; // adds icon code to url
+            
+            var windSpeed = data.list[i].wind.speed * 3.6; // pulls in wind speed + converts to km/hr
+
+            let forecastCard = $(`<div>`)
+            .addClass(`card forecastCard`)
+
+            let forecastDate = $(`<h4>`)
+            .addClass(`forecastDate`).text(`${moment(data.list[i].dt_txt).format(`dddd`)}`)
+
+            let forecastIcon = $(`<img>`)
+            .addClass(`icon`).attr(`src`, iconImg).attr(`alt`, iconTxt)
+
+            let forecastTemp = $(`<span>`)
+            .addClass(`forecastTemp`).text(`Temp: ` + tempC.toFixed(2) + ` °C`)
+
+            let forecastWind = $(`<span>`)
+            .addClass(`forecastWind`).text(`Wind : ` + (data.list[i].wind.speed * 3.6).toFixed(2) + ` km/h`)
+
+            let forecastHumidity = $(`<span>`)
+            .addClass(`forecastHumidity`).text(`Humidity: ` + data.list[i].main.humidity + ` %`)
+
+            // append HTML elements
+            forecastCard.append(forecastDate);
+            forecastCard.append(forecastIcon);
+            forecastCard.append(forecastTemp);
+            forecastCard.append(forecastWind);
+            forecastCard.append(forecastHumidity);
+
+            // append to HTML existing structre
+            forecastEl.append(forecastCard);
+        }
+    };
+});
+}
 
 // function weatherForecast() {
 //   let queryURL = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + lat +'&lon=' + lon +'&appid=' + myApiKey;
